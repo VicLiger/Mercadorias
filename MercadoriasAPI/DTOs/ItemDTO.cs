a7b02a57bffd394c1cd35ae79f1487ab04448379
Victor <victor.liger0800@gmail.com>
2024-06-24 12:19:48 -0300

    Criando DTOs do objeto mercadoria

---
+++ MercadoriasAPI/DTOs/ItemDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace MercadoriasAPI.DTOs
+{
+    public class ItemDTO
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+        public string Descrição { get; set; }
+
+        [Required]
+        public decimal Preço { get; set; }
+
+        [Required]
+        public string codigo { get; set; }
+    }
+}
