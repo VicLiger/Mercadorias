c16ac09f01264a298b9edf1857c264c464f2101d
Victor <victor.liger0800@gmail.com>
2024-07-05 20:46:38 -0300

    Implementando controller de item

--- MercadoriasAPI/Controllers/ItemController.cs
+++ MercadoriasAPI/Controllers/ItemController.cs
@@ -1,9 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MercadoriasAPI.Models;
+using MercadoriasAPI.Repository;
+using MercadoriasAPI.Repository.Interface;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MercadoriasAPI.Controllers
 {
+
+    [ApiController]
+    [Route("[controller]")]
     public class ItemController : ControllerBase
     {
-        
+        private readonly IItemRepository _repository;
+        private readonly ILogger _logger;
+
+        public ItemController(ItemRepository repository, ILogger logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Item>> GetItens()
+        {
+            var categories = _repository.GetFullItens();
+            return Ok(categories);
+        }
     }
 }
--- MercadoriasAPI/Repository/ItemRepository.cs
+++ MercadoriasAPI/Repository/ItemRepository.cs
@@ -1,35 +1,54 @@
-﻿using MercadoriasAPI.Models;
+﻿using MercadoriasAPI.Context;
+using MercadoriasAPI.Models;
 using MercadoriasAPI.Repository.Interface;
+using Microsoft.EntityFrameworkCore;
 
 namespace MercadoriasAPI.Repository
 {
     public class ItemRepository : IItemRepository
     {
-        
-        
+
+        private readonly ItemContext _context;
+
+        public ItemRepository(ItemContext context)
+        {
+            _context = context;
+        }
         public Item Creatitem(Item item)
         {
-            throw new NotImplementedException();
+            if (item is null)
+                throw new ArgumentNullException(nameof(item));
+
+            _context.Itens.Add(item);
+            _context.SaveChanges();
+
+            return item;
         }
 
         public Item DeleteItem(int id)
         {
-            throw new NotImplementedException();
+            var item = _context.Itens.Find(x => x.Id == id);
         }
 
         public IEnumerable<Item> GetFullItens()
         {
-            throw new NotImplementedException();
+            return _context.Itens.ToList();
         }
 
         public Item GetItemById(int id)
         {
-            throw new NotImplementedException();
+            return _context.Itens.FirstOrDefault(x => x.Id == id);
         }
 
         public Item UpdateItem(Item item)
         {
-            throw new NotImplementedException();
+            if (item is null)
+                throw new ArgumentNullException(nameof(item));
+
+            _context.Entry(item).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return item;
         }
     }
 }
